9c569393d0b4ea0be912a05c4700a6e730c4255a
JUDE <judeani60@gmail.com>
2026-01-08 01:09:58 +0100

    Fixed rest_framework_simplejwt spelling in settings

--- SHOPLIFT/settings.py
+++ SHOPLIFT/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framwork_simplejwt',
+    'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'commerce',
     'account',
--- commerce/tests.py
+++ commerce/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+
+from commerce.models import Product, Cart, CartItem
 
 # Create your tests here.
+
+class ProductAPITests(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="Jude", password="password")
+
+        self.client.force_authenticate(user=self.user)
+
+        self.product = Product.objects.create(name='Nike Jordans', description='Kicks on air', price='999.99', category='FW')
+
+    def test_product_create(self):
+        data = {
+            "name":"Nike Jordans", 
+            "description":"Kicks on air", 
+            "price":999.99, 
+            "category":"FW"
+        }
+
+        url = reverse('product-list')
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
